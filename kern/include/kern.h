9ad8af906f5d3dbfdfef660a614f3af05ed22412
plosfas <david.groot@gmail.com>
2013-05-24 15:09:30 -0400

    .

---
+++ kern/include/kern.h
@@ -0,0 +1,15 @@
+#ifndef __KERN_H__
+#define __KERN_H__
+
+#include <values.h>
+#include <task.h>
+
+#define MAXTASKS 100
+
+int *getSP();
+int Create_sys(int priority, void (*code)());
+void syscall_enter();
+void getNextRequest(struct Task *task, struct Request *request);
+void handle(struct Request *request);
+
+#endif
---
+++ kern/kern.c
@@ -0,0 +1,119 @@
+#include <kern.h>
+#include <bwio.h>
+#include <task.h>
+#include <syscall.h>
+#include <userTasks.h>
+#include <queue.h>
+
+static int *freeMemStart;
+static int *kernMemStart;
+static int nextTID;
+static struct Task *active;
+static struct Request *activeRequest;
+static struct PriorityQueue *readyQueue;
+static struct Task *taskArray;
+
+int main() {
+  *((int *)0x28) = (int)syscall_enter;
+  nextTID = 0;
+
+  kernMemStart = getSP();
+  //leave 1KB of stack space for function calls
+  kernMemStart -= 0x400;
+
+  //initialize the readyQueue
+  kernMemStart -= sizeof(struct PriorityQueue);
+  readyQueue = (struct PriorityQueue *)kernMemStart;
+  initQueue(readyQueue);
+
+  //initialize the active request structure
+  kernMemStart -= sizeof(struct Request);
+  activeRequest = (struct Request *)kernMemStart;
+
+  //initialize the array of task destriptors
+  kernMemStart -= MAXTASKS*sizeof(struct Task);
+  taskArray = (struct Task *)kernMemStart;
+
+  //now start available user memory at this point
+  freeMemStart = kernMemStart;
+
+  //create the first user task
+  active = NULL;
+  Create_sys(1, firstTask);
+  active = dequeue(readyQueue);
+  active->state = ACTIVE;
+  while(active != NULL) {
+    getNextRequest(active, activeRequest);
+    handle(activeRequest);
+  }
+
+  return 0;
+}
+
+int Create_sys(int priority, void (*code)()) {
+  struct Task *newTD = &taskArray[nextTID];
+  //initialize user task context
+  newTD->SP = freeMemStart-56;		//loaded during user task schedule
+  newTD->ID = nextTID++;
+  newTD->SPSR = 0xD0;
+  if(active == NULL) {
+    newTD->parentID = -1;
+  }else{
+    newTD->parentID = active->ID;
+  }
+  newTD->returnAddress = (int)code;
+  newTD->state = READY;
+  newTD->priority = priority;
+  newTD->next = NULL;
+  *(newTD->SP + 12) = (int)freeMemStart;	//stack pointer
+  enqueue(readyQueue, newTD, priority);
+
+  freeMemStart -= 0x3E800;	//give each task 1KB of stack space
+
+  return newTD->ID;
+}
+
+void handle(struct Request *request) {
+  switch(request->ID) {
+    case 0:					//Create
+      if(request->arg2 <= freeMemStart) {	//code pointer on the stack
+        *(active->SP) = -3;
+      }else if(request->arg1 >= NUMPRIO || request->arg1 < 0) {	//invalid priority
+	*(active->SP) = -1;
+      }else if(nextTID >= MAXTASKS) {				//out of TDs
+        *(active->SP) = -2;
+      }else{
+        *(active->SP) = Create_sys(request->arg1, (void (*)())request->arg2);
+        active->state = READY;
+        enqueue(readyQueue, active, active->priority);
+        active = dequeue(readyQueue);
+        active->state = ACTIVE;
+      }
+      break;
+    case 1:				//MyTid
+      *(active->SP) = active->ID;
+      active->state = READY;
+      enqueue(readyQueue, active, active->priority);
+      active = dequeue(readyQueue);
+      active->state = ACTIVE;
+      break;
+    case 2:				//MyParentTid
+      *(active->SP) = active->parentID;
+      active->state = READY;
+      enqueue(readyQueue, active, active->priority);
+      active = dequeue(readyQueue);
+      active->state = ACTIVE;
+      break;
+    case 3:				//Pass
+      active->state = READY;
+      enqueue(readyQueue, active, active->priority);
+      active = dequeue(readyQueue);
+      active->state = ACTIVE;
+      break;
+    case 4:				//Exit
+      active->state = ZOMBIE;
+      active = dequeue(readyQueue);
+      active->state = ACTIVE;
+      break;
+  }
+}
