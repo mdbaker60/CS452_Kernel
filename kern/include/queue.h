8f3767f1d3852b77bf4752fc3a1e2790109251e3
plosfas <david.groot@gmail.com>
2013-05-25 17:38:24 -0400

    .

---
+++ kern/include/queue.h
@@ -0,0 +1,28 @@
+#ifndef __QUEUE_H__
+#define __QUEUE_H__
+
+#define NUMPRIO 8
+
+#include <task.h>
+
+static const int LogTable256[256] = 
+{
+#define LT(n) n, n, n, n, n, n, n, n, n, n, n, n, n, n, n, n
+    -1, 0, 1, 1, 2 ,2 ,2 ,2, 3, 3, 3, 3, 3, 3, 3,
+    LT(4), LT(5), LT(5), LT(6), LT(6), LT(6), LT(6),
+    LT(7), LT(7), LT(7), LT(7), LT(7), LT(7), LT(7), LT(7)
+};
+
+struct PriorityQueue {
+  int highPriority;
+  int availablePriorities;
+  struct Task *head[NUMPRIO];
+  struct Task *tail[NUMPRIO];
+};
+
+void initQueue(struct PriorityQueue *queue);
+void enqueue(struct PriorityQueue *queue, struct Task *task, int priority);
+struct Task *dequeue(struct PriorityQueue *queue);
+int queueEmpty(struct PriorityQueue *queue);
+
+#endif
---
+++ kern/queue.c
@@ -0,0 +1,57 @@
+#include <task.h>
+#include <queue.h>
+#include <values.h>
+
+#include <bwio.h>
+
+void initQueue(struct PriorityQueue *queue) {
+  queue->highPriority = -1;
+  queue->availablePriorities = 0;
+  int i;
+  for(i=0; i<NUMPRIO; i++) {
+    (queue->head)[i] = (queue->tail)[i] = NULL;
+  }
+}
+
+void enqueue(struct PriorityQueue *queue, struct Task *task, int priority) {
+  if(priority > queue->highPriority) {
+    queue->highPriority = priority;
+  }
+
+  queue->availablePriorities |= 1 << priority;
+  if((queue->tail)[priority] == NULL) {
+    (queue->tail)[priority] = (queue->head)[priority] = task;
+  }else{
+    (queue->tail)[priority]->next = task;
+    (queue->tail)[priority] = task;
+  }
+}
+
+
+struct Task *dequeue(struct PriorityQueue *queue) {
+  if(queue->highPriority == LogTable256[0]) return NULL;
+
+  struct Task *retVal = (queue->head)[queue->highPriority];
+  if(retVal->next != NULL) {
+    (queue->head)[queue->highPriority] = retVal->next;
+  }else{
+    (queue->head)[queue->highPriority] = (queue->tail)[queue->highPriority] = NULL;
+
+    queue->availablePriorities &= ~(1 << queue->highPriority);
+    int t, tt;	//temporaries
+    queue->highPriority = 0;
+    if((tt = queue->availablePriorities >> 16)) {
+      queue->highPriority = (t = tt >> 8) ? 24 + LogTable256[t] : 16 + LogTable256[tt];
+    }else{
+      queue->highPriority = (t = queue->availablePriorities >> 8) 
+		? LogTable256[t] : LogTable256[queue->availablePriorities];
+    }
+  }
+
+  retVal->next = NULL;
+  return retVal;
+}
+
+int queueEmpty(struct PriorityQueue *queue) {
+  return queue->highPriority == -1;
+}
