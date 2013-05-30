d4569efea236071b9ed8dbe0dc2a8055f57ec5c5
Matt Baker <mdbaker60@gmail.com>
2013-05-30 17:28:05 -0400

    Added logic for Server

--- user/userTasks.c
+++ user/userTasks.c
@@ -1,6 +1,13 @@
 #include <bwio.h>
 #include <syscall.h>
 #include <nameServer.h>
+struct msg{
+	int control;
+	int message;
+
+};
+
+
 void producer() {
   char reply[100];
   char test[10];
@@ -43,27 +50,96 @@ void consumer() {
   }
   Exit();
 }
-void regger(){
-	//Registers with the nameserver
+
+void Server(){
+	int j;
+	int playerTable[100];
+	int playerMove[100];
+	for (j = 0; j < 100; j++){
+		playerMove[j] = -1;
+	}
 	char* who = "RPS Server";
+	int win = 1;
+	int loss = 0;
+	int quit = 2;
+	int src, playRequest = -1;
+	struct msg in;
 	bwprintf(COM2, "Starting server\r");
 	RegisterAs(who);
 	bwprintf(COM2, "PASSING:\r");		
-	Pass();
+	while (1){
+		Receive(&src, (char*) &in, sizeof(struct msg));
+		switch(in.control){
+		  case 0:
+		  	bwprintf(COM2, "RPSServer: Register\r");
+			if (playRequest == -1) playRequest = in.message;
+			else{
+				Reply(playRequest, 0, sizeof(int));
+				Reply(in.message, 0, sizeof(int));
+				bwprintf(COM2, "RPSServer: Matching tids %d:%d", playRequest, in.message);
+				playerTable[playRequest] = in.message;
+				playerTable[in.message] = playRequest;
+				playRequest = -1;
+			}
+		  	break;
+		  case 1:
+			//in.message contains an int representing R,P or S
+			//	R:0
+			//	P:1
+			//	S:2
+			bwprintf(COM2, "RPSServer: Play\r");
+			if (playerMove[playerTable[src]] == -1){//Opponent has not moved yet
+				playerMove[src] = in.message;
+			}
+			else{
+				if ((playerMove[src] == 2 && in.message == 0)|| in.message > playerMoves[src]){
+					Reply(in.message, (char*)&win, sizeof(int));
+					Reply(playerMove[src], (char*)&loss, sizeof(int));
+				}
+				else{
+					
+					Reply(in.message, (char *)&loss, sizeof(int));
+					Reply(playerMove[src], (char *)&win, sizeof(int));
+				}
+				playerMoves[src] = -1;	
+			}
+			break;
+		  case 2:
+			bwprintf(COM2, "RPSServer: Quit\r");
+			Send(playerTable[src], &quit, sizeof(int));
+		}
+	}
 }
-void reqqer(){
+void Client(){
+	int RPSserver;
+	int i;
 	char* who = "RPS Server";
-	char ret[100];
+	struct msg out;
+	int ret;
 	bwprintf(COM2, "Starting client\r");
-	whoIs(who);
-	if(ret == '\0') bwprintf(COM2, "nothing found\r");
+	RPSserver = whoIs(who);
+	out.control = 0;	//Tells the server we want to register
+	out.message = MyTid(); 
+	Send(RPSserver,  (char*)&out, sizeof(struct msg), (char *) &ret, sizeof(int));
+	out.control = 1;
+	int move = 0;
+	for (i = 0; i < 12; i++){
+		//1 -	Win
+		//0 -	Loss
+		out.message = move;	
+		Send(RPSserver, (char*)&out, sizeof(struct msg), (char *) &ret, sizeof(int)); 
+
+	}
+	out.control = 2;
+	Send(RPSserver, (char*)&out, sizeof(struct msg), (char *) &ret, sizeof(int));
+	
 }
 void firstTask() {
  // Create(0, producer);
   //Create(0, consumer);
   Create(2, NSInit);
-  Create(5, regger);
-  Create(5, reqqer);
+  Create(5, Server);
+  Create(5, Client);
   Pass();
   Exit();
 }
