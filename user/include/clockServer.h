46a48d5b215dc4f24ed13c68f185ef48fa59b142
Matt Baker <mdbaker60@gmail.com>
2013-06-05 16:55:30 -0400

    Added defines for case conditionals

--- user/include/clockServer.h
+++ user/include/clockServer.h
@@ -1,6 +1,11 @@
 #ifndef __CLOCKSERVER_H__
 #define __CLOCKSERVER_H__
 
+#define NOTIFIER	0
+#define DELAYUNTIL	1
+#define DELAY		2
+#define GETTIME		3
+
 void CSInit();
 int Delay(int ticks);
 int Time();
