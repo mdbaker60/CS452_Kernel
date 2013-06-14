031d540b851b67107f46038eb96641ecf961bd36
plosfas <david.groot@gmail.com>
2013-06-14 09:41:50 -0400

    added driver

--- user/userTasks.c
+++ user/userTasks.c
@@ -7,18 +7,17 @@
 #include <systemTasks.h>
 #include <io.h>
 
+void driver() {
+  ShutDown();
+}
+
 void firstTask() {
   Create(6, NSInit);
   Create(6, CSInit);
   Create(6, InputInit);
   Create(6, OutputInit);
   Create(0, idleTask);
+  Create(1, driver);
 
-  int i;
-  for(i=0; i<5; i++) {
-    bwgetc(COM2);
-    bwputc(COM2, '*');
-  }
-
-  Shutdown();
+  Destroy(MyTid);
 }
