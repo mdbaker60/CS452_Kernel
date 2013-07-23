d74ecd7fad5360088fdd3afeeea080a00968027e
plosfas <david.groot@gmail.com>
2013-07-23 10:34:16 -0400

    added missing assert file

---
+++ user/include/assert.h
@@ -0,0 +1,11 @@
+#ifndef __ASSERT_H__
+#define __ASSERT_H__
+
+#define assert(CHECK, MSG) do {\
+if(!(CHECK)) {\
+bwprintf(COM2, "\e[1;1f\e[2JAssertion Failure: %s\r", MSG);\
+Shutdown();\
+}\
+}while(0)
+
+#endif
